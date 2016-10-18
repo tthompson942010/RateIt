de4d5dc134ef7b1e882da1ac779086b31bf6ab82
tthompson942010 <tthompson942010@gmail.com>
2016-10-17 20:53:40 -0400

    changed 1 line

--- models/subjects.js
+++ models/subjects.js
@@ -8,6 +8,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        subjects.hasMany(models.comments);
       }
     }
   });
